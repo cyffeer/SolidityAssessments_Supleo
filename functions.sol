cde3e443747263aff5a562ec6715e5b0599ce5c6
Cyfer <130665260+cyffeer@users.noreply.github.com>
2024-10-04 13:04:36 +0800

    Create functions.sol

---
+++ functions.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Functions {
+    
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b <= a, "Subtraction would result in a negative number.");
+        return a - b;
+    }
+
+    
+    function multiply(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+
+    
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b > 0, "Division by zero is not allowed.");
+        return a / b;
+    }
+}
